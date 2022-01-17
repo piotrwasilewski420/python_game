c2cb8f41477e2b5da341f4b94ae854e7b459441a
piotr_wasilewski <p.wasilewski.681@studms.ug.edu.pl>
2022-01-17 10:35:36 +0100

    almost final commit

--- game.py
+++ game.py
@@ -3,13 +3,18 @@
 from pickle import TRUE
 import pygame as py
 from pygame.display import update
+import math
 
 # Game Init
 py.init()
-
+score=0
+font = py.font.Font('freesansbold.ttf', 32)
+text = font.render('GeeksForGeeks', True,66, 66)
+textRect = text.get_rect()
 # Screen Settings
 width=1000
 height=700
+textRect.center = (width // 2, height // 2)
 screen = py.display.set_mode((width, height))
 surface = py.display.get_surface() #get the surface of the current active display
 SurfaceWidth,SurfaceHeight = size = surface.get_width(), surface.get_height()#create an array of surface.width and surface.height
@@ -21,7 +26,7 @@ def player(x,y):
 # UFO
 UFOImg = py.image.load("ufo.png")
 UFOX=width
-UFOY=0
+UFOY=20
 UFOChange=1
 def UFO(x,y):
     screen.blit(UFOImg, (((x-128)/2, y)))
@@ -36,13 +41,32 @@ bulletChange=1
 bulletY=height-128-32+bulletChange
 bulletX=0
 
+# Rectangles
+bulletRect = bulletImg.get_rect(topleft= ((bulletX-27)/2,bulletY-bulletChange))
+UFORect = UFOImg.get_rect(topleft=(UFOX,UFOY))
 # Player Movement
 PlayerChange=0
+
+# Colision Detection
+def isColided(bx,by,ux,uy):
+    if abs(by-uy) < 110 and abs(bx-ux) < 600:
+        global isBulletReady 
+        global bulletChange
+        global score
+        bulletChange=1
+        isBulletReady = False 
+        score+=1
+        print(bx)
+        print()
+        print(ux)
+        print()
+        print(abs(bx-ux))
+       
 # Game Loop
 running = True
-print(SurfaceWidth)
 while running:
     screen.fill((0,0,0))
+    screen.blit(text, textRect)
     for event in py.event.get():
         if event.type == py.QUIT:
             running = False
@@ -50,13 +74,11 @@ while running:
             if event.key == py.K_LEFT:
                 PlayerChange -= 1
             if event.key == py.K_SPACE: 
-                print("bullet")
                 if isBulletReady==False:
                     bulletX=PlayerX
                 isBulletReady=True     
             if event.key == py.K_RIGHT:
                 PlayerChange += 1
-                print(PlayerChange)
         if event.type == py.KEYUP:
             if event.key == py.K_LEFT:
                 PlayerChange = 0
@@ -82,13 +104,20 @@ while running:
     # print(UFOChange)
     UFO(UFOX,UFOY)
     player(PlayerX,height)
+    # Colision
+    isColided((bulletX-27)/2,bulletY-bulletChange,UFOX,UFOY)
     # bullet
-    print((PlayerX-27)/2)
     if isBulletReady == True:
         firBullet((bulletX-27)/2,bulletY-bulletChange)
         bulletChange += 0.5
         if bulletY-bulletChange <= 0:
-            print("tak")
             isBulletReady=False
             bulletChange=1
+    # print("BulletX",(bulletX-27)/2)
+    # print("BulletY",bulletY-bulletChange)
+    # print("UFOY",UFOY)
+    # print("UFOX",UFOX)
+    # print(isBulletReady)
+    # print("-----")
+    print(score)
     py.display.update()
